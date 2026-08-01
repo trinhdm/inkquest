a295d0bf8df6911659a4f2841c2b1f9f1ade5028
Doreen Trinh <doreenmtrinh@gmail.com>
2026-08-01 02:07:26 -0700

    refactor: cleaned up css vars generators

--- src/providers/ThemeProvider/CssVariables/generator.ts
+++ src/providers/ThemeProvider/CssVariables/generator.ts
@@ -1,3 +1,4 @@
+
 import { isObject, toKebabCase } from '@/utils/helpers'
 import type { CSSVars } from '@/types/shared'
 import type { SiteTheme } from '../theme.types'
@@ -31,71 +32,82 @@ const getShorthand = (tag: string, target: Record<string, any>) => {
 	return shorthand.join(' ')
 }
 
-const formatVariable = <V,>({ key, path = '', value }: {
-	key: string
-	path: string
+const formatCssVars = <V,>({ path, prefix, value }: {
+	path: string[]
+	prefix?: string
 	value: V
-}) => {
-	let name = toKebabCase(key)
+}): string => {
+	let name, variable = ''
+	if (!path.length) return variable
 
-	const isPlural = name.endsWith('s') && name.length > 2,
-		isTagGroup = isObject(value) && Object.hasOwn(value, 'tagName'),
-		isNumeric = typeof value === 'number'
-			|| /\d/.test(`${value}`)
-			|| (isObject(value) && Object.values(value).every(v => /\d/.test(`${v}`)))
+	name = toKebabCase(path[0])
 
 	if (name.startsWith('font')) {
-		const replaced = {
+		const substr = {
 			'font-family': '-family',
 			'font-weight': 'font-',
 		}[name]
 
-		if (replaced)
-			name = name.replace(replaced, '')
+		if (substr)
+			name = name.replace(substr, '')
 	}
 
-	if (isPlural && (isTagGroup || isNumeric))
-		name = name.slice(0, -1)
+	if (name.endsWith('s') && name.length > 2) {
+		const isTagGroup = isObject(value) && Object.hasOwn(value, 'tagName'),
+			isNumeric = typeof value === 'number'
+				|| /\d/.test(`${value}`)
+				|| (isObject(value) && Object.values(value).every(v => /\d/.test(`${v}`)))
 
-	const variable = !!path ? `${path}-${name}` : name
+		if (isTagGroup || isNumeric)
+			name = name.slice(0, -1)
+	}
+
+	const parts = [] as string[]
+	variable = '--'
+
+	path[0] = name
+
+	if (prefix) parts.push(prefix)
+	parts.push(...path)
+
+	variable += parts.join('-')
 
 	return variable
 }
 
-export const toCssVars = <T extends Record<string, any>>(
+const generateCssVars = <T extends Record<string, any>>(
 	input: T,
 	prefix: string = ''
 ) => {
-	const generate = <T extends Record<string, any> = SiteTheme>(target: T, path = '') => {
-		let variables = [] as [string, unknown][]
+	const vars: Record<string, string> = {}
 
-		for (let key in target) {
-			const value = target[key]
-			let vname = formatVariable({ key, path, value })
+	const generate = (value: T, path: string[]) => {
+		if (typeof value === 'function') return
+		if (value === undefined) return
 
-			if (typeof value === 'function') continue
+		const args = { path, prefix, value }
+		let name = formatCssVars(args)
 
-			if (isObject(value)) {
-				if (Object.hasOwn(value, 'tagName')) {
-					const tags = Object.keys(value['tagName'])
-
-					for (const tag of tags) {
-						vname = formatVariable({ key: `text-${tag}`, path, value })
-						variables.push([`--${vname}`, getShorthand(tag, value)])
-					}
-				} else {
-					variables = variables.concat(generate(value, vname))
-				}
-			} else {
-				variables.push([`--${vname}`, value])
+		if (isObject(value)) {
+			if (!Object.hasOwn(value, 'tagName')) {
+				Object.entries(value).forEach(([k, v]) => generate(v, [...path, k]))
+				return
 			}
+
+			const tags = Object.keys(value['tagName'])
+			for (const tag of tags) {
+				name = formatCssVars({ ...args, path: ['text', tag] })
+				vars[name] = getShorthand(tag, value)
+			}
+
+			return
 		}
 
-		return variables
+		vars[name] = String(value)
 	}
 
-	const entries = generate(input, prefix)
-	return Object.fromEntries(entries)
+	generate(input, [])
+	return vars as CSSVars
 }
 
 export const themeToCssVars = (
@@ -110,9 +122,9 @@ export const themeToCssVars = (
 		themeName: 'light',
 	}
 
-	const general = toCssVars(theme, prefix),
-		dark = toCssVars(darkTheme, prefix),
-		light = toCssVars(lightTheme, prefix)
+	const general = generateCssVars(theme, prefix),
+		dark = generateCssVars(darkTheme, prefix),
+		light = generateCssVars(lightTheme, prefix)
 
 	const cssVars = { general, dark, light }
 
