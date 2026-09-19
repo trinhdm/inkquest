3df5584aa65eb24eee33191d61e5dec620d4b9c5
Doreen Trinh <doreenmtrinh@gmail.com>
2026-09-19 11:33:02 -0700

    chore: moved RootCxt code into own `context` dir

---
+++ src/lib/component/context/index.ts
@@ -0,0 +1,3 @@
+export { createRootCxt } from './createRootCxt'
+export { renderWithProvider } from './renderWithProvider'
+export type { RootCxtProviderFn, RootCxtProviderProps } from './createRootCxt'
--- src/lib/component/index.ts
+++ src/lib/component/index.ts
@@ -1,3 +1,1 @@
-export { createRootCxt } from './createRootCxt'
-export { renderWithProvider } from './renderWithProvider'
-export type { RootCxtProviderFn, RootCxtProviderProps } from './createRootCxt'
+export * from './context'
