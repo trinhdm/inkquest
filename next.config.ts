328de4ec8c5778fae477cad846e3be6ae9d92bc3
Doreen Trinh <doreenmtrinh@gmail.com>
2026-09-20 20:18:59 -0700

    feat: segment layout + disable turbopack cache to help speed up nextjs compile time

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	// reactCompiler: false,
-	reactCompiler: true,
+	reactCompiler: process.env.NODE_ENV === 'production',
 	typedRoutes: true,
 	experimental: {
 		optimizePackageImports: ['lucide-react'],
+		turbopackFileSystemCacheForDev: false,
 	},
 	// sassOptions: {
 	// 	additionalData: `@use "@/styles/tokens/_index.scss" as *;`,
